501ceb7479d628735db9b5cfe4103d9fb7dc7b13
Jim Boyd <jimb@cabosoft.com>
2016-08-29 19:44:05 -0600

    AfNetworking 3.0 support

--- Forecastr/Forecastr.m
+++ Forecastr/Forecastr.m
@@ -310,12 +310,14 @@ NSString *const kFCNearestStormBearing = @"nearestStormBearing";
     if ([response isKindOfClass:[NSDictionary class]]) {
         NSString *errorMsg = [response objectForKey:@"error"];
         return (errorMsg.length) ? errorMsg : error.localizedDescription;
-    } else if ([response isKindOfClass:[AFHTTPRequestOperation class]]) {
-        AFHTTPRequestOperation *operation = (AFHTTPRequestOperation *)response;
-        NSInteger statusCode = operation.response.statusCode;
-        NSString *errorMsg = [NSHTTPURLResponse localizedStringForStatusCode:statusCode];
-        return [errorMsg stringByAppendingFormat:@" (code %ld)", (long)statusCode];
-    } else {
+    }
+//	else if ([response isKindOfClass:[AFHTTPRequestOperation class]]) {
+//        AFHTTPRequestOperation *operation = (AFHTTPRequestOperation *)response;
+//        NSInteger statusCode = operation.response.statusCode;
+//        NSString *errorMsg = [NSHTTPURLResponse localizedStringForStatusCode:statusCode];
+//        return [errorMsg stringByAppendingFormat:@" (code %ld)", (long)statusCode];
+//    }
+	else {
         return error.localizedDescription;
     }
 }
